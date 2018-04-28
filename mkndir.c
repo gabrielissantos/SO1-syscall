b49c6a3b452e6b6beaa92c4e0cc5317d150572e2
Joao Barbirato <joaobarbirato@gmail.com>
2018-04-28 11:43:47 -0300

    Modificado 'mkndir.c' com loop para criacao. Ler 'TODO's

--- mkndir.c
+++ mkndir.c
@@ -1,5 +1,20 @@
+// Qual biblioteca chama o sys_mkdir??
 #include <syscalls.h>
+#include <linux/linkage.h>
+#include <linux/unistd.h>
+#include <strings.h>
 
-asmlinkage long sys_mkndir(const short int n,const char * nome, mode_t mode){
-	exit(0);
+
+asmlinkage long sys_mkndir(const short int n, const char * nome, mode_t mode){
+	int i;	
+
+	// Por default, se mkdir nao funciona, retorna -1
+	for(i = 0; i < n; i++)
+		/* TODO: Por "i" como char na frente de nome.
+		 * 	 Lembrar de nao substituir o nome
+		 */
+		if(sys_mkdir(nome, mode) == -1) return -1;
+		
+	// Se funciona, retorna 0
+	return 0;
 }
