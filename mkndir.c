ab7d7eee74d36f277cdd611b07e0b338ce92dc3e
Joao Barbirato <joaobarbirato@gmail.com>
2018-04-30 12:34:12 -0300

    Alterado 'mkndir.c' para contagem de digitos e concatenacao

--- mkndir.c
+++ mkndir.c
@@ -3,28 +3,35 @@
 #include <linux/linkage.h>
 #include <linux/unistd.h>
 #include <string.h>
+#include <malloc.h>
 
+int contaDigitos(const int n);
 
 asmlinkage long sys_mkndir(const short int n, const char * nome, mode_t mode){
 	int i;	
+	char * appended = (char*) malloc (len+1);
+	char * index;
 
-	// Por default, se mkdir nao funciona, retorna -1
 	for(i = 0; i < n; i++){
-
-		//tentativa de por "i" como char na frente de nome.
-		char palavra[3]; //numero de pastas de até 3 algarismos
-		int aux = i; //conteudo de aux será transformado em char
-		sprintf(palavra, %i, aux); //transforma aux em palavra
-		strcat(palavra, nome); //concatena 
-		
-
-		/* TODO: Por "i" como char na frente de nome.
-		 * 	 Lembrar de nao substituir o nome
-		 */
-		if(sys_mkdir(nome, mode) == -1) return -1;
-
+		strcpy(appended, nome);
+		index = (char*) malloc (contaDigitos(i)+2);
+		sprintf(index, "(%d)", i+1);
+		strcat(appended, index); //concatena
+		free(index);
+		// Por default, se mkdir nao funciona, retorna -1
+		if(sys_mkdir(appended, mode) == -1) return -1;
 	}
-		
+	free(aux);
 	// Se funciona, retorna 0
 	return 0;
 }
+
+int contaDigitos(const int n){
+	int aux = n;
+	int d = 1;
+	while(aux / 10 > 1){
+		d++;
+		aux = aux / 10;
+	}
+	return(d);
+};
