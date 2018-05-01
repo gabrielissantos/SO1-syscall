f5aec5f49b837a7dbe09b802c0e357bd52328b01
joao barbirato <joaobarbirato@gmail.com>
2018-05-01 20:29:19 -0300

    Alterada syscall e exemplo funcionando na qemu; removido testeAppend

--- ex-mkndir.c
+++ ex-mkndir.c
@@ -2,15 +2,32 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
 
-int main(){
-	char * nomeDir = "diretorio";
-	int r = syscall(357, 10, nomeDir, S_IRUSR | S_IWUSR | S_IXUSR); // tentativa de criacao em modo usuario
-	if(r == -1)
+int main(int argc, char ** argv){
+	// Saia do programa caso exceda o numero de argumentos
+	if(argc != 3){
+		printf("Numero de argumentos (%d) invalido!\n", argc);
+		return -1;
+	}
+
+	int numDir = strtol(argv[2], NULL, 10);
+
+	// Saia do programa caso a ordem dos argumentos esteja trocada
+	if(!numDir){
+		printf("Ordem dos argumentos incorreta!\n");
+		return -1;
+	}
+
+	char nomeDir[100];
+	strcpy(nomeDir, argv[1]);
+
+	int r = syscall(357, numDir, nomeDir, S_IRUSR | S_IWUSR | S_IXUSR); // tentativa de criacao em modo usuario
+	if(r == -1){
 		printf("Erro ao criar o diretorio.\n");
-	if(r == 0)
-		printf("Diretorio %s criado com sucesso!\n");
-	else{
+	}else if(r == 0){
+		printf("Diretorio %s criado com sucesso!\n", nomeDir);
+	}else{
 		printf("Nao foi possivel acessar a chamada.\n");
 		printf("%d\n", r);
 	}
--- mkndir.c
+++ mkndir.c
@@ -4,34 +4,20 @@
 #include <linux/unistd.h>
 #include <linux/string.h>
 
-int contaDigitos(const int n);
-
 asmlinkage long sys_mkndir(const short int n, const char * nome, mode_t mode){
 	int i;
-	int len = strlen(nome);
-	char * appended = (char*) malloc (len+1);
-	char * index;
+	if(strlen(nome) >= 100 || n >= 999) return -1;
+
+	char appended[100];
+	char index[5];
 
 	for(i = 0; i < n; i++){
 		strcpy(appended, nome);
-		index = (char*) malloc (contaDigitos(i)+2);
 		sprintf(index, "(%d)", i+1);
 		strcat(appended, index); //concatena
-		free(index);
 		// Por default, se mkdir nao funciona, retorna -1
 		if(sys_mkdir(appended, mode) == -1) return -1;
 	}
-	free(appended);
 	// Se funciona, retorna 0
 	return 0;
 }
-
-int contaDigitos(const int n){
-	int aux = n;
-	int d = 1;
-	while(aux / 10 > 1){
-		d++;
-		aux = aux / 10;
-	}
-	return(d);
-};
